4c5cc82aaf7114e730b2fc5c6d79d2f327970d6d
bnschussler <59215102+bnschussler@users.noreply.github.com>
2021-05-14 11:21:55 -0500

    Add files via upload

---
+++ inputHandling.js
@@ -0,0 +1,106 @@
+/*For processing strings as numbers*/
+
+// from https://stackoverflow.com/questions/175739/built-in-way-in-javascript-to-check-if-a-string-is-a-valid-number
+function isNumeric(str) {
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+/*Update Things*/
+
+function updatePText(){
+  document.getElementById("p1").value=Math.floor(p1[0]*100)/100;
+  document.getElementById("p2").value=Math.floor(p2[0]*100)/100;
+}
+
+function updateP(){
+  temp=[document.getElementById("p1").value,document.getElementById("p2").value];
+  if(isNumeric(temp[0]) && isNumeric(temp[1])){
+    p1=[temp[0],temp[0]];
+    p2=[temp[1],temp[1]];
+  }
+}
+
+function updateRes(){
+  if(isNumeric(document.getElementById("resX").value) && isNumeric(document.getElementById("resY").value)){
+    res=[canvas.width/document.getElementById("resX").value,canvas.height/document.getElementById("resY").value];
+  }
+}
+
+function updateCanvasSize(){
+  temp=[document.getElementById("canvWidth").value,document.getElementById("canvHeight").value];
+  if(isNumeric(temp[0]) && isNumeric(temp[1])){
+    canvas.width=temp[0];
+    canvas.height=temp[1];
+    updateRes();
+    drawScreen();
+  }
+}
+
+/*Lock Pointer Code*/
+
+//lock pointer code from https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+canvas.requestPointerLock = canvas.requestPointerLock ||
+                            canvas.mozRequestPointerLock;
+
+document.exitPointerLock = document.exitPointerLock ||
+                           document.mozExitPointerLock;
+
+canvas.onclick = function() {
+  canvas.requestPointerLock();
+};
+
+// pointer lock event listeners
+
+// Hook pointer lock state change events for different browsers
+document.addEventListener('pointerlockchange', lockChangeAlert, false);
+document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+
+function lockChangeAlert() {
+  if (document.pointerLockElement === canvas ||
+      document.mozPointerLockElement === canvas) {
+    console.log('The pointer lock status is now locked');
+    document.addEventListener("mousemove", updatePosition, false);
+  } else {
+    console.log('The pointer lock status is now unlocked');  
+    document.removeEventListener("mousemove", updatePosition, false);
+  }
+}
+
+var tracker = document.getElementById('tracker');
+
+function updatePosition(e) {
+  mX += e.movementX;
+  mY += e.movementY;
+}
+
+/*Keyboard Inputs*/
+
+window.addEventListener('keydown',this.keyPressed,false);
+window.addEventListener('keyup',this.keyReleased,false);
+
+function keyPressed(e) {
+  switch(e.key){
+    case('w'):
+      p1[1]++;
+      break;
+    case('s'):
+      p1[1]--;
+      break;
+    case('a'):
+      p2[1]--;
+      break;
+    case('d'):
+      p2[1]++;
+      break;
+    case('x'):
+      alert("Parameters: "+[p1[0],p2[0]]);
+      break;
+  }
+}
+
+function keyReleased(e) {
+
+}
+
